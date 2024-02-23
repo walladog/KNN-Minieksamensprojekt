6ea85da1d2c4ee4e3c834d74eeaced8b0619b1f8
BroderAfBussy <aske.nordfalk@gmail.com>
2024-02-23 10:45:51 +0100

    Create KNN-actual.py

---
+++ applikation/KNN-actual.py
@@ -0,0 +1,13 @@
+
+
+
+class Film:
+    def __init__(self, name, genre, rating, popularity):
+        self.name = name #Filmens navn
+        self.genre = genre #Liste over genrer denne film har.
+        self.rating = rating
+        self.pop = popularity
+    
+    @staticmethod
+    def genreDistance(movie1Genres,movie2Genres):
+        return
