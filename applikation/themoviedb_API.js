278637c13a55827acfd6530342d65f2213e32374
Lucas Ta Jensen <vitusdavid1507@gmail.com>
2024-02-23 10:21:34 +0100

    diller

---
+++ applikation/themoviedb_API.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const readline = require('readline');
+
+// Define the options for the request
+const options = {
+  method: 'GET',
+  url: 'https://urban-dictionary7.p.rapidapi.com/v0/define',
+  headers: {
+    'API-KEY': '',
+    'API-Host': ''
+  }
+};
+
