20226e4aa167afb0cc268dc8ddbdefecc5df4618
BroderAfBussy <aske.nordfalk@gmail.com>
2024-02-24 01:07:30 +0100

    Update KNN-actual.py

--- applikation/KNN-actual.py
+++ applikation/KNN-actual.py
@@ -1,13 +1,28 @@
 
-
+alleGenrer = {}
 
 class Film:
-    def __init__(self, name, genre, rating, popularity):
+    def __init__(self, name, genres, rating, popularity):
         self.name = name #Filmens navn
-        self.genre = genre #Liste over genrer denne film har.
+        self.rawGenres = genres #Liste over genrer denne film har.
+        self.refinedGenres = alleGenrer
         self.rating = rating
         self.pop = popularity
     
+    def generateGenreList(self):
+        for genre in self.rawGenres:
+            if genre['name'] in alleGenrer:
+                self.refinedGenres[genre['name']] = 1
+                return
+            else:
+                alleGenrer[genre['name']] = 0
+                self.refinedGenres[genre['name']] = 1
+
+
+
+            
+
+
     @staticmethod
     def genreDistance(movie1Genres,movie2Genres):
         return
