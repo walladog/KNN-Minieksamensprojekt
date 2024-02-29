aa75bcae069d63b849dfccf7528753e28795a38b
Lucas Ta Jensen <vitusdavid1507@gmail.com>
2024-02-29 13:49:44 +0100

    tingeling

--- applikation/streamlit.py
+++ applikation/streamlit.py
@@ -5,3 +5,9 @@ st.write("""
 # Bing chilling
 Goddav *skøge!*
 """)
+
+x = st.text_input("How big = cock?")
+st.write(f"din pik er {x} lang")
+is_clicked = st.button("click for bitches :eggplant:")
+
+choice = st.radio("kan du lide denne film?", ["Ja","Nej","Har ikke set den"])
