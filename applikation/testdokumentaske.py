94f6b84c0d43dd5b5e7f8bb2128913a04edb1dc5
BroderAfBussy <aske.nordfalk@gmail.com>
2024-02-29 14:06:51 +0100

    bob

--- applikation/testdokumentaske.py
+++ applikation/testdokumentaske.py
@@ -1,3 +1,5 @@
+#Denne fil er lavet til at teste om KNNactual fungerer ordentligt.
+
 from KNNactual import *
 from BetterMovieDB_API import *
 
@@ -9,4 +11,5 @@ filmgøj = FilmListeKlasse()
 filmgøj.addFilm(response['original_title'],response['genre_ids'],response['vote_average'],response['vote_count'])
 filmgøj.addFilm(response2['original_title'],response2['genre_ids'],response2['vote_average'],response2['vote_count'])
 
-print(filmgøj.genreDict)
+filmgøj.generateRefinedGenres()
+print(filmgøj.filmListe[1].refinedGenres)
