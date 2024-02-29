24bd69662ecb826bd1f98e44c2b141d63f1a146d
Lucas Ta Jensen <vitusdavid1507@gmail.com>
2024-02-29 14:06:33 +0100

    noget med noget

--- applikation/streamlit.py
+++ applikation/streamlit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import Themoviedb_API
+from BetterMovieDB_API import *
 
 st.write("""
 # Bing chilling
@@ -10,4 +11,8 @@ x = st.text_input("How big = cock?")
 st.write(f"din pik er {x} lang")
 is_clicked = st.button("click for bitches :eggplant:")
 
+PosterURL = findFilm('forest gump')['poster_path']
+
+st.image(f'https://image.tmdb.org/t/p{PosterURL}', caption='Sunrise by the mountains')
 choice = st.radio("kan du lide denne film?", ["Ja","Nej","Har ikke set den"])
+
