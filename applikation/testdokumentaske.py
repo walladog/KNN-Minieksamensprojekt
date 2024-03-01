0c25de701d34e9e820754b0bced5ccda5f76f9c7
BroderAfBussy <aske.nordfalk@gmail.com>
2024-03-01 08:37:03 +0100

    idk

--- applikation/testdokumentaske.py
+++ applikation/testdokumentaske.py
@@ -3,13 +3,15 @@
 from KNNactual import *
 from BetterMovieDB_API import *
 
-response = findFilm('toy story 2')
+response = findFilm('forrest gump')
 response2 = findFilm('toy story')
+
+print(response)
 
 filmgøj = FilmListeKlasse()
 
 filmgøj.addFilm(response['original_title'],response['genre_ids'],response['vote_average'],response['vote_count'])
 filmgøj.addFilm(response2['original_title'],response2['genre_ids'],response2['vote_average'],response2['vote_count'])
 
-filmgøj.generateRefinedGenres()
-print(filmgøj.filmListe[1].refinedGenres)
+#filmgøj.generateRefinedGenres()
+#print(filmgøj.filmListe[1].refinedGenres)
