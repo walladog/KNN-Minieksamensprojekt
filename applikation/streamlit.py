583c4ea56009bb573b7bf8624d282ef2a9808131
Lucas Ta Jensen <vitusdavid1507@gmail.com>
2024-03-01 11:27:42 +0100

    det virker bedrere jaaa

--- applikation/streamlit.py
+++ applikation/streamlit.py
@@ -1,6 +1,9 @@
 import streamlit as st
-import Themoviedb_API
+from KNNactual import *
 from BetterMovieDB_API import *
+
+filmLiaste = FilmListeKlasse()
+
 
 st.write("""
 # Bing chilling
@@ -9,6 +12,7 @@ Goddav *skøge!*
 
 filmNavn = st.text_input("søg film:")
 
+response = findFilm(f'{filmNavn}')
 PosterURL = findFilm(f'{filmNavn}')['poster_path']
 Overwiew = findFilm(f'{filmNavn}')['overview']
 Title = findFilm(f'{filmNavn}')['original_title']
@@ -16,6 +20,20 @@ Title = findFilm(f'{filmNavn}')['original_title']
 st.write(f"Du har valgt {Title}")
 
 st.image(f'https://image.tmdb.org/t/p/w500{PosterURL}', caption=f'{Overwiew}')
-choice = st.radio("kan du lide denne film?", ["Ja","Nej","Har ikke set den"])
-is_clicked = st.button("Enter")
+choice = st.radio("kan du lide denne film?", ["Ja","Nej"])
+TilføjFilm_clicked = st.button("Tilføj film")
+
+if TilføjFilm_clicked:
+    if choice == 'Ja':
+        st.write(f'filmen "{Title}" er nu tilføjet som en film du kan lide')
+        filmLiaste.addFilm(response['original_title'],response['genre_ids'],response['vote_average'],response['vote_count'], 1)
+    
+    elif choice == 'Nej':
+        st.write(f'filmen "{Title}" er nu tilføjet som en film du ikke kan lide')
+        filmLiaste.addFilm(response['original_title'],response['genre_ids'],response['vote_average'],response['vote_count'], -1)
+
+    st.write(f'{filmLiaste.filmListe}')
+    
+
+
 
